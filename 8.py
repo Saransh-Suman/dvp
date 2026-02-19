43bc076f58bc39de97d41b9e443a34f0ff906a73
Saransh Suman <49954322+Saransh-Suman@users.noreply.github.com>
2026-02-19 20:19:32 +0530

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,14 +1,9 @@
-import plotly.graph_objects as go
+import plotly.express as px
 import pandas as pd
-tips = pd.read_csv("./DataSet/tips.csv")
-fig = go.Figure()
-Scatter = go.Scatter3d(
-    x = tips['total_bill'],
-    y = tips['tip'],
-    z = tips['size'],
-    mode = 'markers'
-)
-fig.add_trace(Scatter)
-fig.update_layout(scene = dict(xaxis_title = 'Total Bill',yaxis_title = 'Tip',zaxis_title = 'Size'))
-fig.update_layout(title = '3D Scatter plot with Tips Dataset')
+
+df = pd.read_csv("./DataSet/tips.csv")
+fig = px.scatter_3d(df, x='total_bill', y='tip', z='size', 
+                    title='3D Scatter plot with Tips Dataset',
+                    labels={'total_bill': 'Total Bill', 'tip': 'Tip', 'size': 'Size'})
+
 fig.write_html("./Externals/8.html")
